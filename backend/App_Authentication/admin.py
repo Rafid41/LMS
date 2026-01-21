96c2e6fe0de9813ed3832a355dec3703166a3aab
Rafid41 <wap.alnahiyan425@gmail.com>
2026-01-21 22:11:07 +0600

    Registration process frontend and backend-2

--- backend/App_Authentication/admin.py
+++ backend/App_Authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from App_Authentication.models import User, UserType, TemporaryRegistration
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserType)
+admin.site.register(TemporaryRegistration)
